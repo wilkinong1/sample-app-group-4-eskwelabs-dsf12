f9398d55d5471a445c8a3ac468aaa460c27febac
wilkinong1 <wilkin.ong@gmail.com>
2023-11-17 20:07:06 +0800

    Update model_files.py

--- model_files.py
+++ model_files.py
@@ -4,10 +4,10 @@ import imblearn
 from nltk.corpus import stopwords
 import contractions
 import nltk
+nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('punkt')
 nltk.download('stopwords')
-nltk.download('wordnet')
 import string
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
